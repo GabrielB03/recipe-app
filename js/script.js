c37003ba004a53b1e3599087e65b54461be5d9b0
GabrielB03 <bielbomfim03@gmail.com>
2025-04-30 10:43:05 -0300

    Initial commit: Recipe App with HTML, CSS and JS

---
+++ js/script.js
@@ -0,0 +1,50 @@
+const apiKey = 'a506c0d2fd024d0395e6a6b9454eab13'; // Change this number to your Spoonacular API Key
+const searchBtn = document.getElementById('search-btn');
+const input = document.getElementById('ingredient-input');
+const recipesContainer = document.getElementById('recipes');
+
+searchBtn.addEventListener('click', () => {
+    const ingredients = input.value.trim();
+    if (ingredients) {
+        fetchRecipes(ingredients);
+    }
+});
+
+async function fetchRecipes(ingredients) {
+    recipesContainer.innerHTML = '<p>Loading recipes...</p>';
+
+    try {
+        const response = await fetch (
+            `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${encodeURIComponent(ingredients)}&number=9&apiKey=${apiKey}`
+        );
+        const data = await response.json();
+        displayRecipes(data);
+    } catch (error) {
+        console.error('Error fetching recipes:', error);
+        recipesContainer.innerHTML = '<p>Failed to load recipes. Please try again.</p>'
+    }
+}
+
+function displayRecipes(recipes) {
+    if (recipes.length === 0) {
+        recipesContainer.innerHTML = '<p>No recipes found for the given ingredients.</p>';
+        return;
+    }
+
+    recipesContainer.innerHTML = '';
+
+    recipes.forEach(recipe => {
+        const recipeCard = document.createElement('div');
+        recipeCard.classList.add('recipe-card');
+
+        recipeCard.innerHTML = `
+          <img src="${recipe.image}" alt="${recipe.title}">
+          <h3>${recipe.title}</h3>
+          <p>Used ingredients: ${recipe.usedIngredientCount}</p>
+          <p>Missed ingredients: ${recipe.missedIngredientCount}</p>
+          <a href="https://spoonacular.com/recipes/${recipe.title.replace(/ /g, '-')}-${recipe.id}" target="_blank">View Recipe</a>
+        `;
+
+        recipesContainer.appendChild(recipeCard);
+    });
+}
